6db1b92f3af1ea2f1eb674b661e0d940fa9f8c54
timurmalkoc <malkoctimurcelik@gmail.com>
2022-10-04 17:27:25 -0400

    working on about section

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,14 +3,17 @@ import './nav.css'
 import { FaHome, FaUserAlt } from 'react-icons/fa'
 import { BsTools } from 'react-icons/bs'
 import { MdContactMail, MdComputer } from 'react-icons/md'
+import { useState } from 'react'
+
 const Nav = () => {
+  const [activeNav, setActiveNav ] = useState('#')
   return (
     <nav>
-      <a href="#"><FaHome/></a>
-      <a href="#about"><FaUserAlt/></a>
-      <a href="#experience"><MdComputer/></a>
-      <a href="#services"><BsTools/></a>
-      <a href="#contact"><MdContactMail/></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === "#" ? 'active' : null}><FaHome/></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === "#about" ? 'active' : null}><FaUserAlt/></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === "#experience" ? 'active' : null}><MdComputer/></a>
+      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === "#services" ? 'active' : null}><BsTools/></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === "#contact" ? 'active' : null}><MdContactMail/></a>
     </nav>
   )
 
