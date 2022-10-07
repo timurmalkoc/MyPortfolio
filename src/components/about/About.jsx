68c369a0f7981cbe23852af48f092cc3934d5140
timurmalkoc <malkoctimurcelik@gmail.com>
2022-10-06 23:46:32 -0400

    experience

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import './about.css'
 import ME from '../../media/about_me.jpg'
-import { FaAward } from 'react-icons/fa'
-import { FiUsers } from 'react-icons/fi'
-import { VscFolderLibrary } from 'react-icons/vsc'
 const About = () => {
   return (
 
       <section id='about'>
-        <h5>Get To Know</h5>
-        <h2>About Me</h2>
+        <div className='about__know'><b>Get To Know</b></div>
+        <div className='about__about-me'>About <b className='about-me'>Me</b></div>
 
         <div className="container about__container">
           <div className="about__me">
@@ -19,25 +16,9 @@ const About = () => {
           </div>
 
           <div className="about__content">
-            <div className="about__cards">
-              <article className="about__card">
-                <FaAward className='about__icon'/>
-                <h5>Experience</h5>
-                <small>8+ Years Working In IT Related</small>
-              </article>
-              <article className="about__card">
-                <FiUsers className='about__icon'/>
-                <h5>Fill later client</h5>
-                <small>300+</small>
-              </article>
-              <article className="about__card">
-                <VscFolderLibrary className='about__icon'/>
-                <h5>Projects</h5>
-                <small>20+ Completed</small>
-              </article>
-            </div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni vel tempora mollitia, quo nisi voluptas omnis, debitis fugiat enim,
-               rerum aliquam aliquid animi eveniet reprehenderit commodi soluta ullam repellendus accusantium.
+            <p>I am a Full-Stack developer in the US. I graduated Sakarya University with a major Computer Systems. I am very passionate 
+              about improving my coding skills & developing applications & websites & test automation. I build WebApps and Websites using Flask
+              and test with Selenuim. Working for myself to improve my skills. Love to build Full-Stack applications.
             </p>
 
             <a href="#contact" className='btn btn-primary'>Let's Talk</a>
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -3,6 +3,32 @@ import './experience.css'
 import { BsPatchCheckFill } from 'react-icons/bs'
 
 function Experience() {
+
+  const front = [
+    { id:1, tool:"HTML",        exp:"Experienced",img:"https://img.icons8.com/color/48/000000/html-5--v1.png" },
+    { id:2, tool:"CSS",         exp:"Experienced",img:"https://img.icons8.com/color/48/000000/css3.png" },
+    { id:3, tool:"JavaScript",  exp:"Experienced",img:"https://img.icons8.com/color/48/000000/javascript--v1.png" },
+    { id:4, tool:"React",       exp:"Experienced",img:"https://img.icons8.com/external-tal-revivo-color-tal-revivo/48/000000/external-react-a-javascript-library-for-building-user-interfaces-logo-color-tal-revivo.png" }
+  ]
+
+  const back = [
+    { id:1, tool:"Python",      exp:"Experienced",  img:"https://img.icons8.com/color/48/000000/python--v1.png" },
+    { id:2, tool:"Java",        exp:"Intermediate", img:"https://img.icons8.com/color/48/000000/java-coffee-cup-logo--v1.png" },
+    { id:3, tool:"ExpressJS",   exp:"Basic",        img:"https://img.icons8.com/fluency/48/000000/node-js.png" },
+    { id:4, tool:"NodeJS",      exp:"Basic",        img:"https://img.icons8.com/color/48/000000/nodejs.png" },
+    { id:4, tool:"C++",         exp:"Basic",        img:"https://img.icons8.com/color/48/000000/c-plus-plus-logo.png" },
+    { id:4, tool:"PostgreSQL",  exp:"Intermediate", img:"https://img.icons8.com/color/48/000000/postgreesql.png" },
+    { id:4, tool:"MySQL",       exp:"Intermediate", img:"https://img.icons8.com/color/48/000000/mysql-logo.png" },
+    { id:4, tool:"MongoDB",     exp:"Intermediate", img:"https://img.icons8.com/color/48/000000/mongodb.png" },
+  ]
+
+  const test = [
+    { id:1, tool:"Selenuim",    exp:"Experienced",  img:"https://img.icons8.com/color/344/selenium-test-automation.png" },
+    { id:2, tool:"Maven",       exp:"Intermediate", img:"https://www.svgrepo.com/show/354051/maven.svg" },
+    { id:3, tool:"Cucumber",    exp:"Experienced",  img:"https://cdn.iconscout.com/icon/free/png-256/cucumber-130-1175236.png" },
+    { id:4, tool:"Postman",     exp:"Experienced",  img:"https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg" },
+    { id:4, tool:"Jenkins",     exp:"Intermediate", img:"https://img.icons8.com/color/344/jenkins.png" }
+  ]
   return (
 
     <section id='experience'>
@@ -10,126 +36,54 @@ function Experience() {
       <h2>My Experience</h2>
 
       <div className="container experience__container">
-        <div className="experience__frontend">
-          <h3>Frontend Development</h3>
+        <div className="experience__sub">
+          <h3 className='experience__type'>Frontend Development</h3>
           <div className="experience__content">
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>HTML</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>CSS</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>JavaScript</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>React</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
+            {
+              front.map(({id, tool,exp,img}) => {
+                return(
+                  <div key={id} className='experience__details'>
+                      <div className='tool__title'>{tool}</div>
+                      <img src={img} alt={tool} className="tool_img"/>
+                      <small className='tool__exp'>{exp}</small>
+                  </div>
+                )
+              })
+            }
           </div>
         </div>
         {/* end of first column */}
-        <div className="experience__backend">
-          <h3>Backend Development</h3>
+        <div className="experience__sub">
+          <h3 className='experience__type'>Backend Development</h3>
           <div className="experience__content">
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>Python</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>Flask</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>Java</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>Postgres</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>MySQL</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>MongoDB</h4>
-                <small className='text-light'>Basics</small>
-              </div>
-            </article>
+            {
+              back.map(({id, tool,exp,img}) => {
+                return(
+                  <div key={id} className='experience__details'>
+                      <div className='tool__title'>{tool}</div>
+                      <img src={img} alt={tool} className="tool_img"/>
+                      <small className='tool__exp'>{exp}</small>
+                  </div>
+                )
+              })
+            }
           </div>
         </div>
         {/* end of second column */}
-        <div className="experience__frontend">
-          <h3>Test Automation</h3>
+        <div className="experience__sub">
+          <h3 className='experience__type'>Test Automation</h3>
           <div className="experience__content">
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>Selenium</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>Cucumber</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>TestNG</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>REST API</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>Jenkins</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
+            {
+              test.map(({id, tool,exp,img}) => {
+                return(
+                  <div key={id} className='experience__details'>
+                      <div className='tool__title'>{tool}</div>
+                      <img src={img} alt={tool} className="tool_img"/>
+                      <small className='tool__exp'>{exp}</small>
+                  </div>
+                )
+              })
+            }
           </div>
         </div>
         {/* end of third column */}
